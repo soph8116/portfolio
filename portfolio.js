10ba935629a16ead7d9a514dee8b5569018fae68
portfolio <soph8116@stud.kea.dk>
2021-01-09 18:06:13 +0100

    om side almost done

--- portfolio.js
+++ portfolio.js
@@ -28,6 +28,7 @@ window.onscroll = function () {
 
 
 function scrollFunction() {
+    console.log("scrollFunction");
     if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
         document.querySelector("#backtotop").style.display = "block";
     } else {
@@ -39,4 +40,29 @@ function scrollFunction() {
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+//Om mig side
+//For at lave en animeret fold ud box tilføjer jeg: max-height: 0, overflow: hidden og transition til: max-height 0.2s ease-out i mit css. Derefter bruger JavaScript til at glide indholdet ned ved at indstille en beregnet maksimalhøjde afhængigt af panelets højde på forskellige skærmstørrelser.
+
+let coll = document.getElementsByClassName("collapsible");
+let i;
+
+//for = loops through a block of code a number of times
+for (i = 0; i < coll.length; i++) {
+
+    coll[i].addEventListener("click", function () {
+
+        //Toggle : tænd/sluk funktion
+
+        this.classList.toggle("active");
+
+        let content = this.nextElementSibling;
+
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
 }
